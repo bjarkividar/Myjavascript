4f43ef9bff62b21ef86eddc111a523be3d8daa11
Eva Sif Einarsdóttir <Eva Sif Einarsdttir>
2017-05-05 14:00:29 +0000

    Tengja File við rétt Project

---
+++ MyJavaScript/Controllers/FilesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyJavaScript.Models;
+using MyJavaScript.Models.Entity;
+
+namespace MyJavaScript.Controllers
+{
+    public class FilesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Files
+        public ActionResult Index(int? id)
+        {
+            /*IEnumerable<Project> pro = db.Projects;
+            IEnumerable <File> result = db.Files.Where(t => pro.Contains(t.ProjectID));
+            IEnumerable<File> result = from fi in db.Files
+                                       where fi.ProjectID == db.Projects.
+            //db.Projects.Where(t => ids.Contains(t.ID));
+            return View(db.Files.ToList());*/
+
+            return View(db.Files.Where(x => x.ProjectID.Equals(id.Value)).ToList());
+        }
+
+        // GET: Files/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: Files/Create
+        public ActionResult Create(int? id)
+        {
+            
+            return View();
+        }
+
+        // POST: Files/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ContentType,Data,ProjectID")] File file)
+        { 
+            if (ModelState.IsValid)
+            {
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(file);
+        }
+
+        // GET: Files/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ContentType,Data,ProjectID")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(file);
+        }
+
+        // GET: Files/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
